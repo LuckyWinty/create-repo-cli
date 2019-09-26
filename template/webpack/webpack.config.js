8aff8fa7b770220048ebcacf03ad72c1769e9087
winty <18814127231@163.com>
2019-09-26 15:24:15 +0800

    feat: remove tpl

--- template/webpack/webpack.config.js
+++
@@ -1,4 +0,0 @@
-const devConfig = require('./webpack/webpack.dev.config.js')
-const prodConfig = require('./webpack/webpack.prod.config')
-
-module.exports = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
